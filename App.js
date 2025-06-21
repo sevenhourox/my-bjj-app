dbe02939a43242ae3b9a105de622c74d691bf1d8
sevenhourox <samuel@gbknox.com>
2025-06-20 23:15:13 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
+i// ✅ All imports should be at the top
 import React from 'react';
-import { Text, View } from 'react-native';
+import { View, Text } from 'react-native';
+import { createClient } from '@supabase/supabase-js';
 
 export default function App() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Test Screen Works</Text>
+      <Text>Hello World</Text>
     </View>
- 
-import { createClient } from '@supabase/supabase-js';
+  );
+}
+
 
 const supabase = createClient('https://lvrukwnevxshclzmperz.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx2cnVrd25ldnhzaGNsem1wZXJ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk5MzY2MDksImV4cCI6MjA2NTUxMjYwOX0.dUh68ZFt3CMbPIDwGwvddxuhrmDNnrRcch2f9XDFoKM');
 
